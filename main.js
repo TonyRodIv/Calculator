ecdb66c5ededfe4788f64a1a531e44ea7b5e408b
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-15 11:53:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,10 +20,49 @@ operationBtn.forEach((i) => {
 
 
 equalsBtn.addEventListener('click', () => {
-    display.value  = eval(display.value);
+    display.value = eval(display.value);
     console.log(display.value)
 });
 
 clearBtn.addEventListener('click', () => {
     display.value = '';
+})
+
+function mudarTema() {
+    document.body.style.setProperty('--BG', '#FFFBFA');
+    document.body.style.setProperty('--text', '#2e2827');
+    document.body.style.setProperty('--displayBG', '#F4DED7');
+    document.body.style.setProperty('--numBTN', '#FBF0ED');
+    document.body.style.setProperty('--numOPE', '#FFDACF');
+    document.body.style.setProperty('--numAC', '#FAE191');
+    document.body.style.setProperty('--equalBTN', '#FFDACF');
+
+    document.body.style.setProperty('--numHOV', '#e4d5d1');
+    document.body.style.setProperty('--opeHOV', '#ebc7bc');
+    document.body.style.setProperty('--acHOV', '#e9d287');
+    document.body.style.setProperty('--equalHOV', '#ebc7bc');
+}
+function mudarTemaGreen() {
+    document.body.style.setProperty('--BG', '#FDFEF7');
+    document.body.style.setProperty('--text', '#373D34');
+    document.body.style.setProperty('--displayBG', '#E2E8DF');
+    document.body.style.setProperty('--numBTN', '#F3F5ED');
+    document.body.style.setProperty('--numOPE', '#DAE7D1');
+    document.body.style.setProperty('--numAC', '#D7EBEF');
+    document.body.style.setProperty('--equalBTN', '#C7EFBB');
+
+    document.body.style.setProperty('--numHOV', '#e6e9df');
+    document.body.style.setProperty('--opeHOV', '#cedbc4');
+    document.body.style.setProperty('--acHOV', '#c7dce0');
+    document.body.style.setProperty('--equalHOV', '#bae0ae');
+}
+
+let checkbox = document.getElementById('checkTHEME')
+
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        console.log(mudarTema())
+    } else{
+        console.log(mudarTemaGreen())
+    }
 })
