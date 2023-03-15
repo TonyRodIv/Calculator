ac14e7ae7c38a6733ded3edaf1b31394b913000d
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-15 10:07:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,18 +7,21 @@ const display = document.querySelector('#display');
 numbersBtn.forEach((i) => {
     i.addEventListener('click', () => {
         display.value += i.innerHTML;
+        console.log(display.value)
     });
 });
 
 operationBtn.forEach((i) => {
     i.addEventListener('click', () => {
         display.value += i.innerHTML;
+        console.log(display.value)
     });
 });
 
 
 equalsBtn.addEventListener('click', () => {
     display.value  = eval(display.value);
+    console.log(display.value)
 });
 
 clearBtn.addEventListener('click', () => {
